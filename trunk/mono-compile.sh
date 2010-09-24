f2f4125b3fc6333c33453ff96ecd90a0d9b8d160
adriano_04 <adriano_04@8e12ecf2-0027-0410-9260-996ebcc6df30>
2010-09-24 14:15:30 +0000

    Fixed file struture, updated config.xml to lastest MyODBC and added revision details to source.

--- trunk/mono-compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd Source
-gmcs -define:MONO -unsafe+ -optimize -target:exe -out:../Server_Mono.exe -win32icon:icon.ico -recurse:*.cs -reference:System.Data -reference:System.Drawing -reference:System.Xml -reference:System.Windows.Forms
-cp config.xml ../config.xml
-cp encryption.dll ../encryption.dll
